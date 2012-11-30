37cc0bea7404ec4f927180ab2497b414acecda70
David Elias <david@david-elias.net>
2012-11-30 06:59:23 +0000

    Allow relative path requires only when using "./" prefix

--- gears/processors/directives.py
+++ gears/processors/directives.py
@@ -41,7 +41,8 @@ class DirectivesProcessor(BaseProcessor):
         self.asset.requirements.add(self.get_asset(*self.find(path)))
 
     def process_require_directory_directive(self, path, recursive=False):
-        path = self.get_relative_path(path, is_directory=True)
+        if path.startswith('./'):
+            path = self.get_relative_path(path)
         list = self.asset.attributes.environment.list(path, self.asset.attributes.mimetype, recursive=recursive)
         for asset_attributes, absolute_path in sorted(list, key=lambda x: x[0].path.split('/')):
             self.asset.requirements.add(self.get_asset(asset_attributes, absolute_path))
@@ -57,16 +58,19 @@ class DirectivesProcessor(BaseProcessor):
         self.asset.dependencies.add(self.find(path)[1])
 
     def find(self, require_path):
-        require_path = self.get_relative_path(require_path)
+        if require_path.startswith('./'):
+            require_path = self.get_relative_path(require_path)
+        require_path = '%s%s' % (require_path, ''.join(self.asset.attributes.extensions))
+
+        'finding:', require_path
+
         asset_attributes = AssetAttributes(self.asset.attributes.environment, require_path)
         return self.asset.attributes.environment.find(asset_attributes, True)
 
-    def get_relative_path(self, require_path, is_directory=False):
+    def get_relative_path(self, require_path):
         require_path = os.path.join(self.asset.attributes.dirname, require_path)
         require_path = os.path.normpath(require_path)
-        if is_directory:
-            return require_path
-        return require_path + ''.join(self.asset.attributes.extensions)
+        return require_path
 
     def get_asset(self, asset_attributes, absolute_path):
         return Asset(asset_attributes, absolute_path, self.asset.calls)
