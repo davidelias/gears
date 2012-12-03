5083ff134cc986d68583cebf8a30137bce0bb1a2
David Elias <david@david-elias.net>
2012-12-03 01:40:18 +0000

    Allow use of "." for relative paths

--- gears/processors/directives.py
+++ gears/processors/directives.py
@@ -21,6 +21,7 @@ class DirectivesProcessor(BaseProcessor):
 
     def __call__(self, asset):
         self.asset = asset
+        self.environment = asset.attributes.environment
         self.parse()
         self.process_directives()
 
@@ -41,10 +42,10 @@ class DirectivesProcessor(BaseProcessor):
         self.asset.requirements.add(self.get_asset(*self.find(path)))
 
     def process_require_directory_directive(self, path, recursive=False):
-        if path.startswith('./'):
+        if path.startswith('.'):
             path = self.get_relative_path(path)
-        list = self.asset.attributes.environment.list(path, self.asset.attributes.mimetype, recursive=recursive)
-        for asset_attributes, absolute_path in sorted(list, key=lambda x: x[0].path.split('/')):
+        paths = self.environment.list(path, self.asset.attributes.mimetype, recursive=recursive)
+        for asset_attributes, absolute_path in sorted(paths, key=lambda x: x[0].path.split('/')):
             self.asset.requirements.add(self.get_asset(asset_attributes, absolute_path))
             self.asset.dependencies.add(os.path.dirname(absolute_path))
 
@@ -58,14 +59,12 @@ class DirectivesProcessor(BaseProcessor):
         self.asset.dependencies.add(self.find(path)[1])
 
     def find(self, require_path):
-        if require_path.startswith('./'):
+        if require_path.startswith('.'):
             require_path = self.get_relative_path(require_path)
         require_path = '%s%s' % (require_path, ''.join(self.asset.attributes.extensions))
 
-        'finding:', require_path
-
-        asset_attributes = AssetAttributes(self.asset.attributes.environment, require_path)
-        return self.asset.attributes.environment.find(asset_attributes, True)
+        asset_attributes = AssetAttributes(self.environment, require_path)
+        return self.environment.find(asset_attributes, True)
 
     def get_relative_path(self, require_path):
         require_path = os.path.join(self.asset.attributes.dirname, require_path)
